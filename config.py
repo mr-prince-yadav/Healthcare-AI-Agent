183243c311e09a4ac5558d43d775116fc604424e
Prince Yadav <py63535@gmail.com>
2026-01-10 01:27:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # config.py
 import streamlit as st
 
-GOOGLE_API_KEY = st.secrets["api"]["google_api_key"]
-RELAY_WEBHOOK_URL = st.secrets["api"]["relay_webhook_url"]
+GOOGLE_API_KEY = st.secrets["api"]["GOOGLE_API_KEY"]
+RELAY_WEBHOOK_URL = st.secrets["api"]["RELAY_WEBHOOK_URL"]
+
